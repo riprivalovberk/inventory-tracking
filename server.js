a2f8124b08037499a3d44a4eb4d7d7462cdcdaf1
Richard Privalov <riprivalov@berkeley.edu>
2022-05-22 18:38:34 -0700

    fixing replit hopefully

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const items = require('./routes/api/items');
 const comments = require('./routes/api/comments');
+const path = require('path');
 
 const app = express();
 var cors = require('cors');
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
+const { builtinModules } = require('module');
 
 mongoose.connect(db)
     .then(() => console.log('Connected to server!'))
@@ -19,6 +21,16 @@ mongoose.connect(db)
 //Routes
 app.use('/api/items', items);
 app.use('/api/comments', comments);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
